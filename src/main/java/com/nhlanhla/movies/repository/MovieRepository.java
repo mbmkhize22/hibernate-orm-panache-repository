ff32bfb9ac650ffc96f1a643d0e711862e7a1e9d
Nhlanhla Mkhize <mbmkhize22@gmail.com>
2022-04-05 15:49:04 +0200

    added swaggerUI

--- src/main/java/com/nhlanhla/movies/repository/MovieRepository.java
+++ src/main/java/com/nhlanhla/movies/repository/MovieRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public class MovieRepository implements PanacheRepository<Movie> {
 
     public List<Movie> findByCountry(String country) {
-        return list("SELECT m FROM Movie m WHERE m.country = ?1 ORDER BY DESC", country);
+        return list("SELECT m FROM Movie m WHERE m.country = ?1", country);
     }
 }
